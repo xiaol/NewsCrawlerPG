73b9920dc4b138be1a8c6ad96d5edb2a6ec4690b
lixianyang <705834854@qq.com>
2016-01-26 22:17:04 +0800

    add start urls for news163, toutiao, yidianzixun

--- News/constans/toutiao.py
+++ News/constans/toutiao.py
@@ -2,7 +2,26 @@
 
 SPIDER_NAME = "spider:toutiao"
 DOMAIN = "http://toutiao.com"
-
+CHANNELS = {
+    "育儿": "news_baby",
+    "汽车": "news_car",
+    "探索": "news_discovery",
+    "娱乐": "news_entertainment",
+    "美文": "news_essay",
+    "时尚": "news_fashion",
+    "财经": "news_finance",
+    "美食": "news_food",
+    "游戏": "news_game",
+    "历史": "news_history",
+    "军事": "news_military",
+    "养生": "news_regimen",
+    "社会": "news_society",
+    "体育": "news_sports",
+    "故事": "news_story",
+    "科技": "news_tech",
+    "旅游": "news_travel",
+    "国际": "news_world",
+}
 
 
 
--- News/constans/yidianzixun.py
+++ News/constans/yidianzixun.py
@@ -2,7 +2,22 @@
 
 SPIDER_NAME = "spider:yidianzixun"
 ARTICLE_URL_TEMPLATE = "http://www.yidianzixun.com/home?page=article&id={docid}"
-
+CHANNELS = {
+    "汽车": "汽车",
+    "时尚": "时尚",
+    "财经": "财经",
+    "趣图": "趣图",
+    "健康": "健康",
+    "搞笑": "搞笑",
+    "互联网": "互联网",
+    "军事": "军事",
+    "科学": "科学",
+    "股票": "股票",
+    "体育": "体育",
+    "科技": "科技",
+    "社会": "社会",
+    "美女": "美女",
+}
 
 
 
