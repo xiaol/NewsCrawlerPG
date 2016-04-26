8016ad3fae2245e659f30589c689c8b0f9f7d01b
Sven Lee <705834854@qq.com>
2016-04-26 17:51:43 +0800

    add grep -v grep to kill process in release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'kill all spider process'
-ps -ewf | grep spider:news | awk '{print $2}' | xargs kill -9
+ps -ewf | grep spider:news | grep -v grep | awk '{print $2}' | xargs kill -9
 
 echo 'remove pyc'
 find -name *.pyc | xargs rm -f
