2e4bb9b8ca1a6269fff9a763d45998d980944328
Sven Lee <705834854@qq.com>
2016-05-05 11:25:16 +0800

    添加spider调试支持

--- test.py
+++ test.py
@@ -7,6 +7,18 @@
 import unittest
 from News.test.static_or_class_methods import TestCleanPostDate
 
-if __name__ == '__main__':
+from scrapy import cmdline
+
+
+def unit_test():
     unittest.main()
+
+
+def debug_spider():
+    argv = "scrapy crawl spider:news:www_jrj_com_cn".split()
+    cmdline.execute(argv)
+
+
+if __name__ == '__main__':
+    debug_spider()
 
