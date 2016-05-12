26515861b16b656e224ec49e36d24b44d4343f28
Sun Laite <sunlt@lieying.com>
2016-05-12 12:01:29 +0800

    修改界面八卦source_url错误

--- News/constans/www_jiemian_com.py
+++ News/constans/www_jiemian_com.py
@@ -34,7 +34,7 @@ CATEGORIES = {
     u'大牌': ('http://www.jiemian.com/lists/25.html', 3, 240, 4),
     u'娱乐圈': ('http://www.jiemian.com/lists/76.html', 3, 240, 4),
     u'视听': ('http://www.jiemian.com/lists/77.html', 30, 240, 4),
-    u'八卦': ('http://www.jiemian.com/lists/88.html', 3, 240, 4),
+    u'八卦': ('http://www.jiemian.com/lists/81.html', 3, 240, 4),
     u'巨星': ('http://www.jiemian.com/lists/11.html', 6, 240, 4),
     u'体育市场': ('http://www.jiemian.com/lists/78.html', 6, 240, 4),
     u'篮球': ('http://www.jiemian.com/lists/80.html', 6, 120, 4),
