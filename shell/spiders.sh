c58f7d546ccfda10b651b131fa2fb21de1be36c2
Sun Laite <sunlt@lieying.cn>
2016-09-07 14:34:36 +0800

    修改启动爬虫

--- shell/spiders.sh
+++ shell/spiders.sh
@@ -6,16 +6,16 @@ curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:y
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:yidianzixun
 
 # 今日头条
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:toutiao
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:toutiao
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:toutiaohao
+#curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:toutiao
+#curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:toutiao
+#curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:toutiaohao
 
 # 网易新闻
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:news163
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:news163
 
 # 微信评论
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:wechat
+# curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:comment:wechat
 
 # 新浪
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:roll_news_sina_com_cn
@@ -24,12 +24,12 @@ curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:t
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:fashion_sina_cn
 
 # 搜狐
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:news_sohu_com
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:www_sohu_com
+# curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:news_sohu_com
+# curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:www_sohu_com
 
 # 腾讯
 curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:news_qq_com
 
 # zaker
-curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:app_myzaker_com
+# curl http://localhost:6888/schedule.json -d project=News -d spider=spider:news:app_myzaker_com
 
